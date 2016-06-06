db452b1b6460bb981f7d654b11961b5ef3fb87d4
Louis Warren <louis@lsw.nz>
2016-06-07 10:59:17 +1200

    Fix trailing whitespace

--- pyaccum.py
+++ pyaccum.py
@@ -7,6 +7,6 @@ def accumulate(accum_type):
             return accum_type(iter(f(*args, **kwds)))
         return inner_wrapper
     return outer_wrapper
-    
+
 def accumulate_star(accum_type):
     return accumulate(lambda x: accum_type(*x))
